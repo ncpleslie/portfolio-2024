0f4de17f5d20e20e4f4083c95fd01ab632847d84
Nick Leslie <ncpleslie@gmail.com>
2023-12-13 16:19:11 -0500

    Adding base files

---
+++ astro.config.mjs
@@ -0,0 +1,23 @@
+import { defineConfig } from "astro/config";
+import vue from "@astrojs/vue";
+import tailwind from "@astrojs/tailwind";
+
+import vercel from "@astrojs/vercel/serverless";
+
+// https://astro.build/config
+export default defineConfig({
+  // Enable Vue to support Vue components.
+  integrations: [vue(), tailwind()],
+  output: "server",
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true,
+    },
+    speedInsights: {
+      enabled: true,
+    },
+    imageService: true,
+    devImageService: "sharp",
+    functionPerRoute: true,
+  }),
+});

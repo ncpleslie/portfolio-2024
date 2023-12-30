98f8b0a8df0c83f444b7b131ef2043d61590eef5
Nick Leslie <ncpleslie@gmail.com>
2023-12-29 19:29:13 -0500

    Fixed icon loading issue

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,14 +3,13 @@ import tailwind from "@astrojs/tailwind";
 import vercel from "@astrojs/vercel/serverless";
 import sitemap from "@astrojs/sitemap";
 import robotsTxt from "astro-robots-txt";
-import preload from "astro-preload";
 import compress from "astro-compress";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://dev.nickleslie.dev",
-  integrations: [tailwind(), sitemap(), robotsTxt(), preload(), compress()],
-  output: "server",
+  integrations: [tailwind(), sitemap(), robotsTxt(), compress()],
+  output: "hybrid",
   adapter: vercel({
     webAnalytics: {
       enabled: true,
